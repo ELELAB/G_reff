15378a775f44e90e9a0102ab9b6ac19b9bb79021
Elena Papaleo <elena@kb-bioinfo01.cancer.dk>
2018-02-14 17:30:12 +0100

    revised README + LICENSE

--- NOEconverter.py
+++ NOEconverter.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python 
+#    Copyright (C) 2018 Matteo Lambrughi, Matteo Tiberti, Maria Francesca Allega, Valentina Sora, Mads Nygaard, Agota Toth, Juan Salamanca Viloria, Emmanuelle Bignon, Elena Papaleo <elenap@cancer.dk>, Computational Biology Laboratory, Danish Cancer Society Research Center, 2100, Copenhagen, Denmark
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#!/usr/bin/python
+#!/usr/bin/env python
+__authors__ = "Mads Nygaard" 
 #Dependencies
 import sys
 import itertools
